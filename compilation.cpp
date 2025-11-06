ac83b0743077c9a981ded718218d8c9179a33db3
Josh Birch <joshuabirch@protonmail.com>
2025-11-06 15:34:24 +0000

    Start chapter 9 on Macros

--- compilation.cpp
+++ compilation.cpp
@@ -2,6 +2,9 @@
 // Created by josh on 11/6/25.
 //
 
+// Use "-save-temps" to save temporary files and "-c" to compile the source files into an object file.
+// the .s file is the assembler instructions, and the .ii file is the modified source code.
+
 #include <iostream>
 using namespace std;
 
--- macros.cpp
+++ macros.cpp
@@ -1,3 +1,24 @@
 //
 // Created by josh on 11/6/25.
 //
+
+// The "#define" directive specifies a macro
+// Macro names traidtioanlly use UPPERCASE
+
+// 
+
+#define BOOK "C++ Programming in Easy Steps"
+#define NUM 200
+#define RULE "***************************"
+
+// Specify the library classes to include the namespace
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << RULE << endl << BOOK << endl << RULE;
+    cout << endl << "NUM is: " << NUM << endl;
+    cout << "Double NUM: " << ((NUM ) * 2) << endl;
+    return 0;
+}
