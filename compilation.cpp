3f3724e793b76a5e25ee637bb2f7c29dca7aa86b
Josh Birch <joshuabirch@protonmail.com>
2025-11-06 15:34:13 +0000

    Start chapter  9 on Macros

---
+++ compilation.cpp
@@ -0,0 +1,12 @@
+//
+// Created by josh on 11/6/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This is a test" << endl;
+    return 0;
+}
---
+++ macros.cpp
@@ -0,0 +1,3 @@
+//
+// Created by josh on 11/6/25.
+//
